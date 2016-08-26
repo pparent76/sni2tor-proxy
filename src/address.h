409617dfb8fc5ae739e051ba084e73bc22863f0c
Remi Gacogne <remi.gacogne@powerdns.com>
2016-08-26 14:51:06 +0200

    Report an error in case of an invalid port, instead of `assert(0)`

--- src/address.h
+++ src/address.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Dustin Lundquist <dustin@null-ptr.net>
+ * Copyright (c) 2013, Dustin Lundquist <dustin@null-ptr.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,17 +23,38 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef PROTOCOL_H
-#define PROTOCOL_H
+#ifndef ADDRESS_H
+#define ADDRESS_H
 
 #include <inttypes.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/socket.h>
 
-struct Protocol {
-    const char *const name;
-    const uint16_t default_port;
-    int (*const parse_packet)(const char*, size_t, char **);
-    const char *const abort_message;
-    const size_t abort_message_len;
-};
+/*
+ * Define size of address buffers for display_address() calls to
+ * be large enough for the maximum domain name and a 5 digit port
+ */
+#define ADDRESS_BUFFER_SIZE 262
+
+struct Address;
+
+struct Address *new_address(const char *);
+struct Address *new_address_sa(const struct sockaddr *, socklen_t);
+struct Address *copy_address(const struct Address *);
+size_t address_len(const struct Address *);
+int address_compare(const struct Address *, const struct Address *);
+int address_is_hostname(const struct Address *);
+int address_is_sockaddr(const struct Address *);
+int address_is_wildcard(const struct Address *);
+const char *address_hostname(const struct Address *);
+const struct sockaddr *address_sa(const struct Address *);
+socklen_t address_sa_len(const struct Address *);
+uint16_t address_port(const struct Address *);
+void address_set_port(struct Address *, uint16_t);
+int address_set_port_str(struct Address *addr, const char* str);
+const char *display_address(const struct Address *, char *, size_t);
+const char *display_sockaddr(const void *, char *, size_t);
+int is_numeric(const char *);
 
 #endif
